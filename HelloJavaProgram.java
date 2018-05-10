e0209f45b0296da5f99e619ed07d3132c0b059a0
Diwakar <diwakarsb11@gmail.com>
2018-05-10 12:30:36 +0530

    added 2 new files hello world program

---
+++ HelloJavaProgram.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class HelloJavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World....");
+
+	}
+
+}
